60506d6b8be208f9e93abdd2835384d78e0c89d4
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-05-30 16:29:39 +0200

    print "Hello Word"

---
+++ src/org/lessons/java/HelloJava.java
@@ -0,0 +1,10 @@
+package org.lessons.java;
+
+import java.util.Arrays;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java" );
+    }
+
+}
